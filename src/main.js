9fea4a23dcc7ea816bcef9e435596b7fd7775576
ycg <122015474@qq.com>
2022-08-12 19:08:49 +0800

    add dark mode

--- src/main.js
+++ src/main.js
@@ -3,7 +3,39 @@ import App from './App.vue'
 import i18n from './i18n/index'
 import router from './router'
 import store from './store'
+import { setMode } from './utils'
 import './style/index.scss'
+
+// 不同模式样式
+let mode = window.localStorage.getItem('mode')
+if (mode) {
+  if (mode === 'dark') {
+    setMode(true)
+  } else {
+    setMode(false)
+  }
+} else {
+  // tp.isDarkMode().then((res) => {
+  //   if (res.result && res.data) {
+  //     setMode(true)
+  //   } else {
+  //     setMode(false)
+  //   }
+  // })
+}
+
+// 当不是 dapp 浏览器的时候
+// if (navigator.userAgent.indexOf('TokenPocket') === -1) {
+//   // 获取系统主题
+//   const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)') // 是深色
+//   if (isDarkTheme.matches) {
+//     // 是深色 主题设置为深色。
+//     document.documentElement.setAttribute('theme', 'dark')
+//   } else {
+//     // 不是深色 主题设置为浅色。
+//     document.documentElement.removeAttribute('theme')
+//   }
+// }
 
 Vue.config.productionTip = false
 
---
+++ src/utils/index.js
@@ -0,0 +1,16 @@
+/** 设置显示模式 */
+export function setMode(mode) {
+  if (mode) {
+    document.documentElement.setAttribute('theme', 'dark')
+  } else {
+    document.documentElement.removeAttribute('theme')
+  }
+}
+// 获取当前模式
+export function getMode() {
+  if (document.documentElement.hasAttribute('theme')) {
+    return false
+  } else {
+    return true
+  }
+}
